b223c525bb13c2fa8158c903cc3e3997d82fb4fc
Lionel Tay <lionel.lt.tay@gmail.com>
2026-03-02 08:38:54 +1100

    docs: Add GitHub link to docs navbar

--- docs/src/app/(docs)/layout.tsx
+++ docs/src/app/(docs)/layout.tsx
@@ -15,6 +15,7 @@ export default function Layout({ children }: { children: ReactNode }) {
             ZAIKit
           </>
         ),
+        githubUrl: "https://github.com/lioneltay/zaikit",
       }}
     >
       {children}
